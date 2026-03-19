11e576e8eaaf3eb807dc5f270deba48b2843e8e9
Max Ichajaya <max.ichajaya@gmail.com>
2026-03-19 09:29:03 -0500

    fix: update document api controller

--- app/Models/Ocupacion.php
+++ app/Models/Ocupacion.php
@@ -8,7 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Ocupacion extends Model
 {
     use HasFactory;
-    
+
     protected $connection = "pgsql";
     protected $table = 'ocupacion';
+
+    protected $fillable = [
+        'id',           // Necesario porque lo estás forzando desde la API
+        'name',         // Corresponde a character varying(100)
+        'descripcion',  // Corresponde a text
+        'isactive',     // Corresponde a boolean
+    ];
 }
